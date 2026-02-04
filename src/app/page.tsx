f53c1051c15eb7784ff9db8077d2603c3587696f
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-04 19:31:48 +0700

    [DEV] update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            One of the greatest mistakes is to judge policies and programs by their intentions rather their results.
+            When software fails, organizations fail.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Milton Friedman (1912-2004)
+            Three years in ERP development taught me to build software with real-world consequences.
           </p>
         </div>
       </main>
